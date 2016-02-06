0fa5866a80557a908e4d4c60f2cff1077007adfb
Alessandro Tegner <alessandro.tegner@gmail.com>
2016-02-06 10:33:12 -0800

    Fixed typo

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,6 +1,6 @@
 Rails.application.config.action_mailer.tap do |action_mailer|
-  configs = Rails.application.secrets.action_mailer.with_indifferent_access
+  configs = Rails.application.secrets.action_mailer
 
-  action_mailer.default_url_options = configs[:default_url_options]
-  action_mailer.smtp_settings = configs[:smtp_settings]
+  action_mailer.default_url_options = configs['default_url_options']
+  action_mailer.smtp_settings = configs['smtp_settings']
 end
