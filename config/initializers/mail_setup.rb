9119003f1e1bb5d66ee413b70f31712321a12119
Alessandro Tegner <alessandro.tegner@gmail.com>
2016-02-06 10:31:37 -0800

    Fixed mail setup accessor

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,5 +1,5 @@
 Rails.application.config.action_mailer.tap do |action_mailer|
-  configs = Rails.application.secrets.action_mailer
+  configs = Rails.application.secrets.action_mailer.with_indifferent_access
 
   action_mailer.default_url_options = configs[:default_url_options]
   action_mailer.smtp_settings = configs[:smtp_settings]
