c0bba441d8426376a3b2a4bc638ac058919c761b
Alessandro Tegner <alessandro.tegner@gmail.com>
2016-02-06 07:49:47 -0800

    Added mail setup

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module Passwd
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.action_mailer.default_url_options = {host: 'localhost'}
   end
 end
---
+++ config/initializers/mail_setup.rb
@@ -0,0 +1,8 @@
+Rails.application.config.action_mailer.default_url_options = {
+  host: 'localhost', port: 3000
+}
+
+Rails.application.config.action_mailer.smtp_settings = {
+  address: 'localhost',
+  port: 1025
+}
